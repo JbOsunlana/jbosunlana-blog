4aa5dc4c4ddbaf04152bd4072f255948e7e9a5e1
Jubril Osunlana <logon2jubril@gmail.com>
2021-12-15 13:47:01 +0100

    My Blog

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('delete_post/<int:pk>/', BlogDeleteView.as_view(), name = 'delete_post'),
+    path('edit_post/<int:pk>/', BlogUpdateView.as_view(), name = 'edit_post'),
+    path('new_post/', BlogCreateView.as_view(), name = 'new_post'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name = 'post_detail'),
+    path('', BlogListView.as_view(), name = 'home'),
+    
+]
